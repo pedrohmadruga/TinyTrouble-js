6e0e36a19a7cb7bbcd91ba73ba87e7305849836e
pedrohmadruga <pedrohfonseca01@gmail.com>
2025-11-08 22:46:55 -0300

    fix :bug: Adjusting canvas size, favicon, adjusting speed so it doesn't move faster on diagonals

--- scripts/utils.js
+++ scripts/utils.js
@@ -9,4 +9,10 @@ export function handlePlayerMovement() {
     if (player.keyPressed.left)  player.xSpeed -= speed;
     if (player.keyPressed.up)    player.ySpeed -= speed;
     if (player.keyPressed.down)  player.ySpeed += speed;
+
+    // Adjust player movement so that it does not move faster diagonally
+    if (player.xSpeed !== 0 && player.ySpeed !== 0) {
+        player.xSpeed *= Math.SQRT1_2; // 1/sqrt(2)
+        player.ySpeed *= Math.SQRT1_2; // 1/sqrt(2)
+    }
 }
